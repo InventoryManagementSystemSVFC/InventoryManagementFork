e6e2bdfab86767530bb825d1c51a888c86cadbd2
thisisnotsnorlax <kelvinsoliman5@gmail.com>
2024-09-17 10:22:37 +0800

    calculator 8th commit

---
+++ AngularComponents/src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+
+
+      num1: number = 0;
+      num2: number = 0;
+      result: number = 0;
+      
+      addition() { 
+
+        this.result = this.num1 + this.num2;
+      }
+
+      subtraction() { 
+
+        this.result = this.num1 - this.num2;
+
+      }
+
+      division() { 
+
+        this.result = this.num1 / this.num2;
+
+      }
+
+      multiplication() { 
+
+        this.result = this.num1 * this.num2;
+
+      }
+
+
+
+
+
+}
