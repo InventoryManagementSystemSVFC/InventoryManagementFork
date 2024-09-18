defee86ec2710e63b7ec8e21a83b5421d0e18a2f
thisisnotsnorlax <kelvinsoliman5@gmail.com>
2024-09-18 21:40:29 +0800

    showdate 13th commit

---
+++ AngularComponents/src/app/components/showdate/showdate.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showdate',
+  templateUrl: './showdate.component.html',
+  styleUrl: './showdate.component.css'
+})
+
+export class ShowdateComponent {
+
+  currentDate: Date = new Date();
+  
+  isCliked: boolean = false;
+
+  
+
+
+  showDate() { 
+
+      if(this.isCliked == false ) { 
+        this.isCliked = true;
+      }
+      else { 
+        this.isCliked = false;
+      }
+
+
+
+  }
+
+
+}
