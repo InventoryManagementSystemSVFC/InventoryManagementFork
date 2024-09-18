3f13aada69b41f3a0c9e913962d9b2cbf86f2139
thisisnotsnorlax <kelvinsoliman5@gmail.com>
2024-09-18 14:30:39 +0800

    evenoddchecker 11th commit

---
+++ AngularComponents/src/app/components/evenoddchecker/evenoddchecker.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evenoddchecker',
+  templateUrl: './evenoddchecker.component.html',
+  styleUrl: './evenoddchecker.component.css'
+})
+
+
+export class EvenoddcheckerComponent {
+
+    number: number = 0;
+    
+    
+}
