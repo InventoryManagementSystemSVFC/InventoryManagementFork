46091b7293fc4a18b81510f23aa391ce2779f4d9
thisisnotsnorlax <kelvinsoliman5@gmail.com>
2024-09-20 14:01:07 +0800

    random number generator  27th commit

---
+++ AngularComponents/src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomnumbergenerator',
+  templateUrl: './randomnumbergenerator.component.html',
+  styleUrl: './randomnumbergenerator.component.css'
+})
+export class RandomnumbergeneratorComponent {
+
+
+  min: number = 1;
+  max: number = 100; 
+  randomNumber: number | null = null;
+
+  generateRandomNumber(): void {
+    this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+  }
+
+}
